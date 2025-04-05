5ffcd9291f05e11286c1fa4e05ea1bd5592959b6
dmitry.nuzhdin <mr.dmitrynuzhdin@yandex.ru>
2025-04-05 23:12:36 +0200

    Adjustments to panel, LCSC bom

--- fabrication.sh
+++ fabrication.sh
@@ -1,2 +1,3 @@
 kikit panelize -p kikit_preset.json bldc_servo_v1.kicad_pcb panel.kicad_pcb
 kikit fab jlcpcb --assembly --schematic bldc_servo_v1.kicad_sch panel.kicad_pcb fabrication
+kicad-cli sch export bom --fields 'LCSC,${QUANTITY}' --group-by 'LCSC' bldc_servo_v1.kicad_sch -o fabrication/LCSC-bom.csv
